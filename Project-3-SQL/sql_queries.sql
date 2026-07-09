eaa7c2d9062b0908f88c1abce4e5edc6b92e44b4
test-Sergey <jilk15bk@mail.ru>
2026-07-10 02:28:34 +0300

    Update sql_queries.sql

--- Project-3-SQL/sql_queries.sql
+++ Project-3-SQL/sql_queries.sql
@@ -1,1 +1,17 @@
+-- Показать всех пользователей
+SELECT * FROM users;
 
+-- Показать только имена пользователей
+SELECT name FROM users;
+
+-- Найти пользователей из Москвы
+SELECT * FROM users
+WHERE city = 'Москва';
+
+-- Найти пользователей старше 25 лет
+SELECT * FROM users
+WHERE age > 25;
+
+-- Отсортировать пользователей по возрасту
+SELECT * FROM users
+ORDER BY age;
